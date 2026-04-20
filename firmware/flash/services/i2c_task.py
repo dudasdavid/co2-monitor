4762848aac3aecf029f323d81c2e3eed423b603d
David Dudas <david.dudas@outlook.com>
2026-04-20 19:04:48 +0200

    add breathing animation on feedback LED and change the timing of sensor readings

--- firmware/flash/services/i2c_task.py
+++ firmware/flash/services/i2c_task.py
@@ -8,15 +8,26 @@ from drivers import ahtx0 as athx0_driver
 from drivers import scd4x as scd4x_driver
 from drivers import ds3231 as ds3231_driver
 from drivers import bmp280 as bmp280_driver
-from drivers2 import pca9685 as pca9685_driver
-from drivers2 import drv2605 as drv2605_driver
-from drivers2 import sps30 as sps30_driver
+from drivers import pca9685 as pca9685_driver
+from drivers import drv2605 as drv2605_driver
+from drivers import sps30 as sps30_driver
 import math
 
 # ---- Global variables ----
 import shared_variables as var
 
-log = Logger("i2c", debug_enabled=True)
+log = Logger("i2c", debug_enabled=False)
+
+# 128-step breathing table, values 1..100
+BREATH_TABLE = [
+  50,52,55,57,60,62,65,67,70,72,75,77,79,82,84,86,
+  88,90,92,94,95,97,98,99,100,100,100,100,99,98,97,95,
+  94,92,90,88,86,84,82,79,77,75,72,70,67,65,62,60,
+  57,55,52,50,47,45,42,40,37,35,32,30,28,25,23,21,
+  19,17,15,13,11,9,8,6,5,3,2,1,0,0,0,0,
+  1,2,3,5,6,8,9,11,13,15,17,19,21,23,25,28,
+  30,32,35,37,40,42,45,47
+]
 
 def is_time_diff_over_threshold(ntp_time, rtc_time, threshold_seconds=60):
     """
@@ -70,6 +81,41 @@ def compensate_humidity(rh_raw: float, t_raw: float, t_cal: float) -> float:
 
     # clamp to physical limits
     return max(0.0, min(100.0, rh_cal))
+
+def convert_hsv2rgb(h,s,v):
+    """
+    Convert HSV (Hue 0–360, Saturation 0–100, Value 0–100)
+    to RGB (each 0–4000)
+    """
+    s /= 100.0
+    v /= 100.0
+
+    if s == 0:
+        r = g = b = int(v * 4000)
+        return (r, g, b)
+
+    h = h % 360
+    h_div = h / 60
+    i = int(h_div)
+    f = h_div - i
+    p = v * (1 - s)
+    q = v * (1 - s * f)
+    t = v * (1 - s * (1 - f))
+
+    if i == 0:
+        r, g, b = v, t, p
+    elif i == 1:
+        r, g, b = q, v, p
+    elif i == 2:
+        r, g, b = p, v, t
+    elif i == 3:
+        r, g, b = p, q, v
+    elif i == 4:
+        r, g, b = t, p, v
+    else:
+        r, g, b = v, p, q
+
+    return (int(r * 4000), int(g * 4000), int(b * 4000))
 
 async def i2c_task(period = 1.0):
     #Init
@@ -132,30 +178,31 @@ async def i2c_task(period = 1.0):
 
     # Initialize SPS30 PM driver
     sps30 = sps30_driver.SPS30(i2c1)
-    print("Firmware version:", sps30.firmware_version())
-    print("Product type:", sps30.product_type())
-    print("Serial number:", sps30.serial_number())
-    print("Status register:", sps30.read_status_register())
-    print("Auto cleaning interval:", sps30.read_auto_cleaning_interval(), "s")
-    #print("Set auto cleaning interval...")
+    log.info("[SPS30] Firmware version:", sps30.firmware_version())
+    log.debug("[SPS30] Product type:", sps30.product_type())
+    log.debug("[SPS30] Serial number:", sps30.serial_number())
+    log.debug("[SPS30] Status register:", sps30.read_status_register())
+    log.debug("[SPS30] Auto cleaning interval:", sps30.read_auto_cleaning_interval(), "s")
+    #log.info("[SPS30] Set auto cleaning interval...")
     #sps30.write_auto_cleaning_interval_days(2)
     #await asyncio.sleep(10)
-    #print("Auto cleaning interval:", sps30.read_auto_cleaning_interval(), "s")
+    #log.info("[SPS30] Auto cleaning interval:", sps30.read_auto_cleaning_interval(), "s")
     #await asyncio.sleep(5)
-    print("Starting measurement...")
+    log.debug("[SPS30] Starting measurement...")
     sps30.start_measurement()
     #await asyncio.sleep(5)
-    #print("Read data...")
-    #print(sps30.get_measurement())
+    #log.info("[SPS30] Read data...")
+    #log.info("[SPS30]", sps30.get_measurement())
     
     i = 0
+    led_idx = 0
 
     #Run    
     while True:
         
         i+=1
-        # Only scan devices in every 10th loop
-        if i % 10 == 0:
+        # Only scan devices in every 50th loop which is 5s
+        if i % 50 == 0:
             devices = i2c1.scan()
             var.system_data.i2c_devices = devices
             
@@ -223,136 +270,170 @@ async def i2c_task(period = 1.0):
             except:
                 var.system_data.i2c_status_pca9685 = "PCA9685 is NOT found at 0x40"
                 
+            # SPS30
+            try:
+                idx = devices.index(0x69)
+                devices.pop(idx)
+                var.system_data.i2c_status_sps30 = "SPS30 is online at 0x69"
+            except:
+                var.system_data.i2c_status_sps30 = "SPS30 is NOT found at 0x69"
+                
             var.system_data.i2c_status_unknown = devices
 
-        lux = veml7700_sensor.read_lux()
-        log.debug("[VEML7700] Lux", lux)
-        
-        if lux is not None:
-            lux_cal = 3.0 * lux - 0
-            var.sensor_data.lux_veml7700 = lux_cal
-        else:
-            var.sensor_data.lux_veml7700 = 0
-                
-        temp = aht21_sensor.temperature + var.aht21_temp_offset
-        rh = aht21_sensor.relative_humidity
-        log.debug("[AHT21] temperature:", temp)
-        log.debug("[AHT21] humidity:", rh)
-        
-        #var.sensor_data.temp_aht21 = temp if temp is not None else 0
-        # Use temperature sensor calibration here
-        if temp is not None:
-            temp_cal = 1.04 * temp - 10.2
-            var.sensor_data.temp_aht21 = temp_cal
-        else:
-            temp_cal = 0.69
-            var.sensor_data.temp_aht21 = temp_cal
-        
-        #var.sensor_data.humidity_aht21 = rh if rh is not None else 0
-        if rh is not None:
-            rh_cal = compensate_humidity(rh, temp, temp_cal)
-            var.sensor_data.humidity_aht21 = rh_cal
-        else:
-            var.sensor_data.humidity_aht21 = 0
-        
-        aqi, tvoc, eco2, temp, rh, eco2_rating, tvoc_rating = ens160_sensor.read_air_quality()
+        # Only read sensors in every 10th loop which is 1s
+        if i % 10 == 0:
+            lux = veml7700_sensor.read_lux()
+            #log.debug("[VEML7700] Lux", lux)
+            
+            if lux is not None:
+                lux_cal = 3.0 * lux - 0
+                var.sensor_data.lux_veml7700 = lux_cal
+            else:
+                var.sensor_data.lux_veml7700 = 0
+                    
+            temp = aht21_sensor.temperature + var.aht21_temp_offset
+            rh = aht21_sensor.relative_humidity
+            #log.debug("[AHT21] temperature:", temp)
+            #log.debug("[AHT21] humidity:", rh)
+            
+            #var.sensor_data.temp_aht21 = temp if temp is not None else 0
+            # Use temperature sensor calibration here
+            if temp is not None:
+                temp_cal = 1.04 * temp - 10.2
+                var.sensor_data.temp_aht21 = temp_cal
+            else:
+                temp_cal = 0.69
+                var.sensor_data.temp_aht21 = temp_cal
+            
+            #var.sensor_data.humidity_aht21 = rh if rh is not None else 0
+            if rh is not None:
+                rh_cal = compensate_humidity(rh, temp, temp_cal)
+                var.sensor_data.humidity_aht21 = rh_cal
+            else:
+                var.sensor_data.humidity_aht21 = 0
+            
+            aqi, tvoc, eco2, temp, rh, eco2_rating, tvoc_rating = ens160_sensor.read_air_quality()
 
-        #log.debug("[ENS160] temperature:", temp)
-        #log.debug("[ENS160] humidity:", rh)
-        log.debug("[ENS160] AQI:", aqi)
-        log.debug("[ENS160] TVOC:", tvoc, "-", tvoc_rating)
-        log.debug("[ENS160] eCO2:", eco2, "-", eco2_rating)
-        
-        #var.sensor_data.temp_ens160 = temp if temp is not None else 0
-        #var.sensor_data.humidity_ens160 = rh if rh is not None else 0
-        var.sensor_data.aqi_ens160 = aqi if aqi is not None else 0
-        var.sensor_data.tvoc_ens160 = tvoc if tvoc is not None else 0
-        var.sensor_data.tvoc_rating_ens160 = tvoc_rating if tvoc_rating is not None else "N/A"
-        var.sensor_data.eco2_ens160 = eco2 if eco2 is not None else 0
-        var.sensor_data.eco2_rating_ens160 = eco2_rating if eco2_rating is not None else "N/A"
-        
-        co2 = scd4x.co2
-        temp = scd4x.temperature
-        rh = scd4x.relative_humidity
-        log.debug("[SCD41] CO2:", co2)
-        log.debug("[SCD41] temperature:", temp)
-        log.debug("[SCD41] humidity:", rh)
-        var.sensor_data.co2_scd41 = co2 if co2 is not None else 0
-        
-        if temp is not None:
-            temp_cal = 0.98 * temp - 6.8
-            var.sensor_data.temp_scd41 = temp_cal
-        else:
-            temp_cal = 0.69
-            var.sensor_data.temp_scd41 = temp_cal
-        
-        if rh is not None:
-            rh_cal = compensate_humidity(rh, temp, temp_cal)
-            var.sensor_data.humidity_scd41 = rh_cal
-        else:
-            var.sensor_data.humidity_scd41 = 0
-        
-        if co2 == None:
-            var.system_data.feedback_led = "off"
-        elif co2 < 1000:
-            var.system_data.feedback_led = "green"
-        elif co2 < 1500:
-            var.system_data.feedback_led = "yellow"
-        else:
-            var.system_data.feedback_led = "red"
-        
-        if i % 10 == 5:
+            #log.debug("[ENS160] temperature:", temp)
+            #log.debug("[ENS160] humidity:", rh)
+            #log.debug("[ENS160] AQI:", aqi)
+            #log.debug("[ENS160] TVOC:", tvoc, "-", tvoc_rating)
+            #log.debug("[ENS160] eCO2:", eco2, "-", eco2_rating)
+            
+            #var.sensor_data.temp_ens160 = temp if temp is not None else 0
+            #var.sensor_data.humidity_ens160 = rh if rh is not None else 0
+            var.sensor_data.aqi_ens160 = aqi if aqi is not None else 0
+            var.sensor_data.tvoc_ens160 = tvoc if tvoc is not None else 0
+            var.sensor_data.tvoc_rating_ens160 = tvoc_rating if tvoc_rating is not None else "N/A"
+            var.sensor_data.eco2_ens160 = eco2 if eco2 is not None else 0
+            var.sensor_data.eco2_rating_ens160 = eco2_rating if eco2_rating is not None else "N/A"
+            
+            co2 = scd4x.co2
+            temp = scd4x.temperature
+            rh = scd4x.relative_humidity
+            #log.debug("[SCD41] CO2:", co2)
+            #log.debug("[SCD41] temperature:", temp)
+            #log.debug("[SCD41] humidity:", rh)
+            var.sensor_data.co2_scd41 = co2 if co2 is not None else 0
+            
+            if temp is not None:
+                temp_cal = 0.98 * temp - 6.8
+                var.sensor_data.temp_scd41 = temp_cal
+            else:
+                temp_cal = 0.69
+                var.sensor_data.temp_scd41 = temp_cal
+            
+            if rh is not None:
+                rh_cal = compensate_humidity(rh, temp, temp_cal)
+                var.sensor_data.humidity_scd41 = rh_cal
+            else:
+                var.sensor_data.humidity_scd41 = 0
+            
+            if co2 == None:
+                var.system_data.feedback_led = "off"
+            elif co2 < 1000:
+                var.system_data.feedback_led = "green"
+            elif co2 < 1500:
+                var.system_data.feedback_led = "yellow"
+            else:
+                var.system_data.feedback_led = "red"
+            
             var.system_data.time_rtc = ds3231.datetime()
-            log.debug("[DS3231] RTC datetime:", var.system_data.time_rtc)
+            #log.debug("[DS3231] RTC datetime:", var.system_data.time_rtc)
             
             if is_time_diff_over_threshold(var.system_data.time_ntp, var.system_data.time_rtc, 60):
                 log.warning("[DS3231] RTC time needs to be updated from NTP time!", var.system_data.time_ntp)
                 ds3231.datetime(var.system_data.time_ntp)
+            
+            pressure = bmp280.pressure
+            temp = bmp280.temperature
+            #log.debug("[BMP280] pressure:", pressure)
+            #log.debug("[BMP280] temperature:", temp)
+            var.sensor_data.pressure_bmp280 = pressure if pressure is not None else 0
+            
+            if temp is not None:
+                temp_cal = 0.98 * temp - 5.6
+                var.sensor_data.temp_bmp280 = temp_cal
+            else:
+                var.sensor_data.temp_bmp280 = 0
         
-        pressure = bmp280.pressure
-        temp = bmp280.temperature
-        log.debug("[BMP280] pressure:", pressure)
-        log.debug("[BMP280] temperature:", temp)
-        var.sensor_data.pressure_bmp280 = pressure if pressure is not None else 0
+            # Mass density is in ug/m3
+            particles = sps30.get_measurement()
+            #log.debug("[SPS30] particle mass density:", particles["mass_density"])
+            var.sensor_data.pm10_sps30 = particles["mass_density"]["pm10"]
+            var.sensor_data.pm4_sps30 = particles["mass_density"]["pm4.0"]
+            var.sensor_data.pm2_5_sps30 = particles["mass_density"]["pm2.5"]
+            var.sensor_data.pm1_sps30 = particles["mass_density"]["pm1.0"]
         
-        if temp is not None:
-            temp_cal = 0.98 * temp - 5.6
-            var.sensor_data.temp_bmp280 = temp_cal
-        else:
-            var.sensor_data.temp_bmp280 = 0
+        #---------------
+        # LED animation
+        #---------------
+        
+        # LUT sinusoidal breathing animation
+        v_breath = BREATH_TABLE[led_idx]
+        led_idx += 2
+        if led_idx >= len(BREATH_TABLE):
+            led_idx = 0
+            
+        scale = 1 # TODO: scale based on lux
+        v_breath_scaled = v_breath * scale
+        
+        # To avoid low yellow turning to red
+        if v_breath_scaled < 0.5:
+            v_breath_scaled = 0.5
         
         if var.system_data.feedback_led == "green":
-            pca9685.duty(0, 0)
-            pca9685.duty(1, 4000)
-            pca9685.duty(2, 0)
-        elif var.system_data.feedback_led == "yellow":
-            pca9685.duty(0, 0)
-            pca9685.duty(1, 1000)
-            pca9685.duty(2, 4000)
-        elif var.system_data.feedback_led == "red":
-            pca9685.duty(0, 0)
-            pca9685.duty(1, 0)
-            pca9685.duty(2, 4000)
-        elif var.system_data.feedback_led == "blue":
-            pca9685.duty(0, 4000)
-            pca9685.duty(1, 0)
-            pca9685.duty(2, 0)
-        elif var.system_data.feedback_led == "off":
-            pca9685.duty(0, 0)
-            pca9685.duty(1, 0)
-            pca9685.duty(2, 0)
-        else: # Default white
-            pca9685.duty(0, 1000)
-            pca9685.duty(1, 1000)
-            pca9685.duty(2, 1000)
+            h = 120
+            s = 100
             
-        # Mass density is in ug/m3
-        particles = sps30.get_measurement()
-        log.debug("[SPS30] particle mass density:", particles["mass_density"])
-        var.sensor_data.pm10_sps30 = particles["mass_density"]["pm10"]
-        var.sensor_data.pm4_sps30 = particles["mass_density"]["pm4.0"]
-        var.sensor_data.pm2_5_sps30 = particles["mass_density"]["pm2.5"]
-        var.sensor_data.pm1_sps30 = particles["mass_density"]["pm1.0"]
+
+        elif var.system_data.feedback_led == "yellow":
+            h = 48
+            s = 100
+            
+        elif var.system_data.feedback_led == "red":
+            h = 0
+            s = 100
+
+        elif var.system_data.feedback_led == "blue":
+            h = 210
+            s = 100
+
+        elif var.system_data.feedback_led == "off":
+            h = 0
+            s = 0
+            v_breath_scaled = 0
+
+        else: # Default white
+            h = 0
+            s = 0
+            
+
+        rgb = convert_hsv2rgb(h, s, v_breath_scaled)
+        
+        pca9685.duty(0, rgb[2])
+        pca9685.duty(1, rgb[1])
+        pca9685.duty(2, rgb[0])
         
         var.system_data.i2c_task_timestamp = time.time()
         
